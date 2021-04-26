456397cb1330fe17e060742cd2dc7e5d47b7e3f8
Mustafa kunwa <m.kunwa52@gmail.com>
2021-04-26 12:53:34 +0530

    Component Test cases written

---
+++ src/app/quote/quote.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuoteComponent } from './quote.component';
+
+
+
+@NgModule({
+  declarations: [QuoteComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class QuoteModule { }
