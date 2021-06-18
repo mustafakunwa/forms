a888d479cb905c714b6cfb7f6c86ebd271665177
Mustafa kunwa <m.kunwa52@gmail.com>
2021-06-18 20:08:35 +0530

    created bootstrap carousel for plan card

--- src/app/plan-subscription/plan-subscription.component.ts
+++ src/app/plan-subscription/plan-subscription.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-plan-subscription',
@@ -11,7 +12,32 @@ export class PlanSubscriptionComponent implements OnInit {
     sms: 20000,
     days: 30,
   };
-  constructor() {}
 
-  ngOnInit(): void {}
+  showNavigationArrows = true;
+  showNavigationIndicators = true;
+  items: number = 5;
+
+  cards:any[]=[1,2,3,4,5,7,8,9]
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (window.innerWidth < 400) this.items = 1;
+    else if (window.innerWidth < 600) this.items = 2;
+    else if (window.innerWidth < 900) this.items = 3;
+    else if (window.innerWidth < 1200) this.items = 4;
+    else if (window.innerWidth < 1500) this.items = 5;
+    else this.items = 6;
+  }
+
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+  }
+  ngOnInit(): void {
+    this.onResize('')
+  }
+
+  getSlideCount(){
+    return Math.ceil(36/this.items)
+  }
 }
--- src/app/plan-subscription/plan-subscription.module.ts
+++ src/app/plan-subscription/plan-subscription.module.ts
@@ -4,10 +4,10 @@ import { PlanSubscriptionComponent } from './plan-subscription.component';
 import { CardModule } from '../card/card.module';
 import { PlanCardComponent } from './plan-card/plan-card.component';
 import { ButtonModule } from '../button/button.module';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 @NgModule({
   declarations: [PlanSubscriptionComponent, PlanCardComponent],
   exports: [PlanSubscriptionComponent],
-  imports: [CommonModule, CardModule, ButtonModule],
+  imports: [CommonModule, CardModule, ButtonModule,NgbModule],
 })
 export class PlanSubscriptionModule {}
