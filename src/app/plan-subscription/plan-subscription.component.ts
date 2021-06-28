14853c25590fc254b383d7f3946bad0156c3b5da
Mustafa kunwa <m.kunwa52@gmail.com>
2021-06-28 15:34:04 +0530

    Added custom carousel

--- src/app/plan-subscription/plan-subscription.component.ts
+++ src/app/plan-subscription/plan-subscription.component.ts
@@ -16,8 +16,11 @@ export class PlanSubscriptionComponent implements OnInit {
   showNavigationArrows = true;
   showNavigationIndicators = true;
   items: number = 5;
+  slideIndex = 1;
+  isInitialize: boolean = false;
+  animation: string = '';
 
-  cards:any[]=[1,2,3,4,5,7,8,9]
+  cards: any[] = [1, 2, 3, 4, 5, 7, 8, 9];
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
@@ -26,7 +29,8 @@ export class PlanSubscriptionComponent implements OnInit {
     else if (window.innerWidth < 900) this.items = 3;
     else if (window.innerWidth < 1200) this.items = 4;
     else if (window.innerWidth < 1500) this.items = 5;
-    else this.items = 6;
+    else this.items = 5;
+    this.isInitialize && this.showSlides(this.slideIndex);
   }
 
   constructor(config: NgbCarouselConfig) {
@@ -34,10 +38,49 @@ export class PlanSubscriptionComponent implements OnInit {
     config.showNavigationIndicators = true;
   }
   ngOnInit(): void {
-    this.onResize('')
+    this.onResize('');
   }
 
-  getSlideCount(){
-    return Math.ceil(36/this.items)
+  ngAfterViewInit() {
+    this.showSlides(this.slideIndex);
+    this.isInitialize = true;
+  }
+
+  buy(event) {}
+
+  getSlideCount() {
+    return Math.ceil(36 / this.items);
+  }
+
+  plusSlides(n) {
+    this.animation = n > 0 ? 'slideIn' : 'slideOut';
+    this.showSlides((this.slideIndex += n));
+  }
+
+  currentSlide(n) {
+    this.animation = n > this.slideIndex  ? 'slideIn' : 'slideOut';
+    this.showSlides((this.slideIndex = n));
+  }
+
+  showSlides(n) {
+    var i;
+    var slides: any = document.getElementsByClassName('mySlides');
+    var dots = document.getElementsByClassName('dot');
+    if (n > slides.length) {
+      this.slideIndex = 1;
+      this.animation="slideIn"
+    }
+    if (n < 1) {
+      this.slideIndex = slides.length;
+      this.animation="slideOut"
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[this.slideIndex - 1].style.display = 'block';
+    dots[this.slideIndex - 1].className += ' active';
   }
 }
